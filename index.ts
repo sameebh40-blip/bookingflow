1afe813d326fda99fb420c93f6d59444702b6832
newly.dev <api@newly.dev>
2026-06-20 16:03:25 +0000

    fix: Remove broken require('react') from index.ts

--- index.ts
+++ index.ts
@@ -1,19 +1,3 @@
-// Strip Newly edit-mode debug props (__sourceLocation etc.) from DOM host elements on web
-if (typeof document !== 'undefined') {
-  const _React = require('react');
-  const _origCreateElement = _React.createElement;
-  _React.createElement = function(type: any, props: any, ...children: any[]) {
-    if (props && typeof type === 'string') {
-      const filtered: Record<string, any> = {};
-      for (const key in props) {
-        if (!key.startsWith('__')) filtered[key] = props[key];
-      }
-      return _origCreateElement(type, filtered, ...children);
-    }
-    return _origCreateElement(type, props, ...children);
-  };
-}
-
 // Initialize Newly console log capture before anything else
 import './utils/errorLogger';
 
