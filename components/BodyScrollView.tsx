66459dbbda7cc8b01c83a78049e0c65ac3068fe2
newly.dev <api@newly.dev>
2026-06-20 15:52:20 +0000

    fix: Filter __-prefixed props in BodyScrollView and AnimatedPressable

--- components/BodyScrollView.tsx
+++ components/BodyScrollView.tsx
@@ -2,13 +2,16 @@ import { forwardRef } from "react";
 import { ScrollView, ScrollViewProps } from "react-native";
 
 export const BodyScrollView = forwardRef<any, ScrollViewProps>((props, ref) => {
+  const safeProps = Object.fromEntries(
+    Object.entries(props as Record<string, unknown>).filter(([k]) => !k.startsWith('__'))
+  ) as ScrollViewProps;
   return (
     <ScrollView
       automaticallyAdjustsScrollIndicatorInsets
       contentInsetAdjustmentBehavior="automatic"
       contentInset={{ bottom: 0 }}
       scrollIndicatorInsets={{ bottom: 0 }}
-      {...props}
+      {...safeProps}
       ref={ref}
     />
   );
